a97f36bbaf76cfcce0990aa19852356ca5359fe1
Hugh Pyle <hughpyle@gmail.com>
2020-12-25 18:53:27 -0500

    Update for 2021 moon images

--- current_moon.js
+++ current_moon.js
@@ -3,19 +3,33 @@
 /*
 ======================================================================
 current_moon.js
-from: https://svs.gsfc.nasa.gov/vis/a000000/a004700/a004768/current_moon.js
+from:
+(2020) https://svs.gsfc.nasa.gov/vis/a000000/a004700/a004768/current_moon.js
+(2021) https://svs.gsfc.nasa.gov/vis/a000000/a004800/a004874/current_moon.js
 
 Include this in the head node of the page.
 ====================================================================== */
 
 const moon_domain = "https://svs.gsfc.nasa.gov";
-const moon_path = "/vis/a000000/a004700/a004768/";
-const moon_year = 2020;
-const moon_febdays = 29;
-const moon_nimages = 8784;
+
+const moon_data = {
+   2020: {
+      "year": 2020,
+      "path": "/vis/a000000/a004700/a004768/",
+      "febdays": 29,
+      "nimages": 8784
+   },
+   2021: {
+      "year": 2021,
+      "path": "/vis/a000000/a004800/a004874/",
+      "febdays": 28,
+      "nimages": 8760
+   }
+}
 
 /* write once, read many, so that the image and the stats are for
    the same date/time */
+var moon_path;
 var moon_imagenum;
 
 
@@ -31,13 +45,15 @@ start of the year.  Otherwise it's 1.
 function get_moon_imagenum(now)
 {
    var year = now.getUTCFullYear();
-   if ( year != moon_year ) {
+   if ( !(year in moon_data)) {
+      moon_path = moon_data[2020]["path"]
       moon_imagenum = 1;
       return false;
    }
    var janone = Date.UTC( year, 0, 1, 0, 0, 0 );
+   moon_path = moon_data[year]["path"]
    moon_imagenum = 1 + Math.round(( now.getTime() - janone ) / 3600000.0 );
-   if ( moon_imagenum > moon_nimages ) moon_imagenum = moon_nimages;
+   if ( moon_imagenum > moon_data[year]["nimages"] ) moon_imagenum = moon_data[year]["nimages"];
    return false;
 }
 
